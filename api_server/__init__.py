b482da13be72ebc46408c5c06ee15652d73f0ef9
Zadigo <pendenquejohn@gmail.com>
2024-08-24 23:05:06 +0200

    Get debug from env

--- api_server/__init__.py
+++ api_server/__init__.py
@@ -1,6 +1,12 @@
+import os
 import dotenv
 import pathlib
 
 BASE_PROJECT = pathlib.Path(__file__).parent.absolute()
 
 dotenv.load_dotenv(BASE_PROJECT / '.env')
+
+
+def get_debug():
+    debug = os.getenv('DEBUG')
+    return True if debug == '1' else False
--- api_server/app.py
+++ api_server/app.py
@@ -1,13 +1,14 @@
 import os
 
-from quart import Quart, jsonify
+from quart import Quart, jsonify, render_template
 from quart_cors import cors
 
-from api_server import BASE_PROJECT
+from api_server import BASE_PROJECT, get_debug
 
 app = Quart(__name__, root_path=BASE_PROJECT)
 app_cors = cors(
     app,
+    allow_credentials=True,
     allow_origin=[
         'http://127.0.0.1:8000'
     ]
@@ -16,10 +17,9 @@ app_cors = cors(
 app_cors.config.update(SECRET_KEY=os.getenv('SECRET_KEY'))
 
 
-@app.route('/home')
+@app.route('/')
 def home():
-    return jsonify({'state': True})
-
+    return render_template('site.html')
 
 if __name__ == '__main__':
-    app.run(debug=os.getenv('DEBUG', False))
+    app.run(debug=True)
