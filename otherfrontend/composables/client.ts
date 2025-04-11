1583b7bcfc8bfd2444236ffc92b6382dd5c930a9
Zadigo <pendenquejohn@gmail.com>
2025-04-11 16:36:05 +0200

    Refactor client object

--- otherfrontend/composables/client.ts
+++ otherfrontend/composables/client.ts
@@ -22,9 +22,10 @@ export function inProduction() {
 /**
  * A function used in order to get the correct domain
  * to which a request should be sent (Django, Quart or Flask)
- * 
- * @param path The path to the endpoint to use
- * @param altDomain Alternative odmain to the one registered in useRuntimeConfig.public.prodUrl
+ *
+ * @param altDomain Production domain used if inProduction() is true
+ * @param [websocket=false] Create a domain for websocket connection
+ * @param [port=8000] Change the default port
  */
 export function getDomain(altDomain?: string | null | undefined, websocket: boolean = false, port: number = 8000): string {
     let domain = '127.0.0.1'
@@ -56,11 +57,11 @@ export function getDomain(altDomain?: string | null | undefined, websocket: bool
 /**
  * Function that returns an url ready to be used with any Django or Quart
  * websocket endpoint
- * 
+ *
  * @param path The path to the endpoint to use
  * @param altDomain Alternative odmain to the one registered in useRuntimeConfig.public.prodUrl
  * @param [port=8000] The port of the backend (development only)
- */
+*/
 export function getWebsocketUrl(path: string, altDomain?: string | null | undefined, port: number = 8000): string {
     const domain = getDomain(altDomain, true, port)
     const url = new URL(path, domain)
@@ -70,6 +71,9 @@ export function getWebsocketUrl(path: string, altDomain?: string | null | undefi
 /**
  * Function used to create a basic axios client that
  * can be used to send api requests
+ * 
+ * @param altDomain Alternative odmain to the one registered in useRuntimeConfig.public.prodUrl
+ * @param [port=8000] The port of the backend (development only)
  */
 export function createSimpleClient(altDomain?: string | null | undefined, port: number = 8000) {
     return axios.create({
@@ -82,7 +86,13 @@ export function createSimpleClient(altDomain?: string | null | undefined, port: 
 
 /**
  * Function that adds authentication intercepors on the base client
- * in order to send and handle authenticated requests 
+ * in order to send and handle authenticated requests
+ * 
+ * @param client The client to decorate with the intereceptors
+ * @param access Access token
+ * @param refresh Refresh token
+ * @param refreshCallback Callback function used to return the refresh token
+ * @param errorCallback Callback function used on error
  */
 function authenticationInterceptors(client: AxiosInstance, access?: string | null | undefined, refresh?: string | null | undefined, refreshCallback?: (token: string) => void, errorCallback?: (error: AxiosError) => void) {
     client.interceptors.request.use(
@@ -141,6 +151,9 @@ function authenticationInterceptors(client: AxiosInstance, access?: string | nul
  * It allows us to invoke composables such as "useCookies" from
  * vueuse and other composables that require client sotrages to
  * be fully available
+ * 
+ * @param altDomain Production domain used if inProduction() is true
+ * @param [port=8000] Change the default port
  */
 export function useAxiosClient(altDomain?: string | null | undefined, port: number = 8000) {
     const client = createSimpleClient(altDomain, port)
@@ -155,6 +168,11 @@ export function useAxiosClient(altDomain?: string | null | undefined, port: numb
 /**
  * Same as useAxiosClient but adds a layer of authentication
  * that with access and refresh tokens for protected views
+ * 
+ * @param access Access token
+ * @param refresh Refresh token
+ * @param altDomain Production domain used if inProduction() is true
+ * @param [port=8000] Change the default port
  */
 export function useAuthenticatedAxiosClient(access: string | null | undefined, refresh: string | null | undefined) {
     const { client } = useAxiosClient()
@@ -169,6 +187,12 @@ export function useAuthenticatedAxiosClient(access: string | null | undefined, r
  * Composable used to get an Axios instance on the server side.
  * The storages are not directly accessible and required special
  * SSR methods provided by Nuxt in order to be accessed
+ * 
+ * @param client The client to decorate with the intereceptors
+ * @param access Access token
+ * @param refresh Refresh token
+ * @param refreshCallback Callback function used to return the refresh token
+ * @param errorCallback Callback function used on error
  */
 export function useServerAxiosClient(access?: string, refresh?: string, refreshCallback?: (token: string) => void, errorCallback?: (error: AxiosError) => void) {
     const client = createSimpleClient()
