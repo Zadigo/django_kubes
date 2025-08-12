c044b76e392cefe9ea895f20cea9a3cbf6552ec1
Zadigo <pendenquejohn@gmail.com>
2025-08-13 00:19:20 +0200

    Remove analytics

--- frontend/src/plugins/index.ts
+++ frontend/src/plugins/index.ts
@@ -1,30 +1,14 @@
-import { App } from 'vue'
 import { useAuthenticatedAxiosClient } from './client'
 
-export * from './client'
-export * from './VueAnalytics'
+import type { App } from 'vue'
 
-import { createAnalytics } from './VueAnalytics'
+export * from './client'
 
 export default function installPlugins() {
   return {
     install(app: App) {
       const { authenticatedClient: client } = useAuthenticatedAxiosClient()
       app.config.globalProperties.$djangoClient = client
-
-      const plugin = createAnalytics({
-        id: 'G-123',
-        tags: [
-          {
-            id: 'G-234',
-            config: {
-              currency: 'EUR'
-            }
-          }
-        ]
-      })
-
-      app.use(plugin)
     }
   }
 }
