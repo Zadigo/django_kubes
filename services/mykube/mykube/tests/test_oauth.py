b3f3212f1ced6fe5dee0a7eee161b23a98819667
Zadigo <pendenquejohn@gmail.com>
2026-07-30 08:56:35 +0200

    Add oauth test

--- services/mykube/mykube/tests/test_oauth.py
+++ services/mykube/mykube/tests/test_oauth.py
@@ -4,16 +4,25 @@ import pytest
 from django.contrib.auth import get_user_model
 from django.test.client import Client
 from faker import Faker
+from oauth2_provider.generators import generate_client_secret
 from oauth2_provider.models import Application
 
 faker = Faker()
 
+CLIENT_SECRET: str = None
+
+def generate_client_secret_value() -> str:
+    global CLIENT_SECRET
+    if CLIENT_SECRET is None:
+        CLIENT_SECRET = generate_client_secret()
+    return CLIENT_SECRET
 
 @pytest.fixture
 def oauth_client():
     return Application.objects.create(
         name="Test Application",
         client_type=Application.CLIENT_CONFIDENTIAL,
+        client_secret=generate_client_secret_value(),
         authorization_grant_type=Application.GRANT_CLIENT_CREDENTIALS,
         redirect_uris="http://localhost",
     )
@@ -36,14 +45,12 @@ def test_django_oauth2_backend(user, oauth_client):
     oauth_client.user = user
     oauth_client.save()
 
-    credentials = f'{oauth_client.client_id}:{oauth_client.client_secret}'
+    credentials = f'{oauth_client.client_id}:{CLIENT_SECRET}'
     b64_credentials = base64.b64encode(credentials.encode('utf-8'))
 
     response = client.post(
         '/o/token/', 
-        data={
-            'grant_type': 'client_credentials',
-        },
+        data='grant_type=client_credentials',
         content_type='application/x-www-form-urlencoded',
         HTTP_CACHE_CONTROL='no-cache',
         HTTP_AUTHORIZATION=f'Basic {b64_credentials.decode()}'
@@ -51,4 +58,4 @@ def test_django_oauth2_backend(user, oauth_client):
 
     data = response.json()
     assert 'error' not in data
-    print(response.json())
+    assert 'access_token' in data
